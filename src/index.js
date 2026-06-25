50b9284c23be748eb0e60a94e8fcbc6a191a26cc
MRdjenssen <sam123janssen@gmail.com>
2026-06-25 16:41:47 +0200

    Route todo page from app entrypoint

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import TodoPage from './TodoPage';
+
+const normalizedPath = window.location.pathname.replace(/\/+$/, '') || '/';
+const RootComponent = normalizedPath === '/todo' ? TodoPage : App;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RootComponent />
   </React.StrictMode>
 );
